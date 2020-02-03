2b3311b962162417112849162356a094b31bdc37
Jordi Flores <jordi.flores@mecalux.com>
2020-02-03 12:41:34 +0100

    fix para ejecuttar con angular y con start electron

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'electron-angular'`, () => {
+  it(`should have as title 'electron-angular-demo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('electron-angular');
+    expect(app.title).toEqual('electron-angular-demo');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to electron-angular!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to electron-angular-demo!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { IpcRenderer } from 'electron';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'electron-angular';
+
+  title = 'electron angular demo';
+  private ipc: IpcRenderer;
+
+  bikes = ['cannondale', 'trek', 'specialized', 'evil', 'orbea', 'bh', 'mondraker', 'propain', 'raaw'];
+
+  constructor() {
+    if((<any>window).require) {
+      try {
+        this.ipc = (<any>window).require('electron').ipcRenderer;
+      } catch (e) {
+        throw e;
+      }
+    } else {
+      console.warn('App not running inside Electron!');
+    }
+  }
+
+  openModal() {
+    console.log('Open a modal');
+    this.ipc.send('openModal');
+  }
 }
+
